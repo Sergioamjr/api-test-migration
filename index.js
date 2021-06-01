b2eda878bea8e01a0cca2a75bc4e32df99d2be16
Sérgio Júnior <sergioamjr91@gmail.com>
2021-06-01 15:12:05 -0300

    Creating an app

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+var express = require("express");
+var app = express();
+const port = process.env.PORT || 4000;
+const message = process.env.MESSAGE;
+
+app.get("/test", (req, res) => {
+  res.send(`World! ${message}`);
+});
+
+app.listen(port, () => {
+  console.log("server ok");
+});
